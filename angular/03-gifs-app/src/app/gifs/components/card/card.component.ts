ee37d1eb18c66cda7993750d69a9cf8622f2d41f
guille3222 <guillermo.ruiz.cortina.martinez@gmail.com>
2024-11-16 23:51:41 +0100

    seccion 8

--- angular/03-gifs-app/src/app/gifs/components/card/card.component.ts
+++ angular/03-gifs-app/src/app/gifs/components/card/card.component.ts
@@ -11,6 +11,6 @@ export class CardComponent implements OnInit {
   @Input() public gif!: Gif;
 
   ngOnInit(): void {
-    if (!this.gif) throw new Error('Gif is required');
+    // if (!this.gif) throw new Error('Gif is required');
   }
 }
--- angular/03-gifs-app/src/app/shared/components/lazy-image/lazy-image.component.ts
+++ angular/03-gifs-app/src/app/shared/components/lazy-image/lazy-image.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import 'animate.css';
 
 @Component({
   selector: 'shared-lazy-image',
@@ -10,8 +11,32 @@ export class LazyImageComponent implements OnInit {
 
   @Input() alt: string = ''
 
+  @Input() loading: "eager" | "lazy" = "lazy";
+
+  @ViewChild('img') public img!: ElementRef<HTMLImageElement>;
+
+  protected hasLoaded = false;
+
+  protected imageWidth = 200;
+
+  protected imageHeight = 200;
+
   ngOnInit(): void {
     if (!this.url) throw new Error('URL property is required.');
+    // const img = new Image();
+    // img.src = this.url;
+    // img.onload = () => {
+    //   this.imageWidth = img.naturalWidth;
+    //   this.imageHeight = img.naturalHeight;
+    //   this.showImg = true;
+    // };
   }
-  
+
+  onLoad() {
+    this.hasLoaded = true;
+    const img = this.img.nativeElement;
+    this.imageWidth = img.width;
+    this.imageHeight = img.height;
+  }
+
 }
