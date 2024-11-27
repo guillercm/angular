00ba40cd9c07836e1983047bd14f6c1f667e8653
guillermo <guillermo.ruiz.cortina.martinez@gmail.com>
2024-11-27 17:30:22 +0100

    seccion 11

--- angular/05-pipesApp/src/app/products/pages/basics-page/basics-page.component.ts
+++ angular/05-pipesApp/src/app/products/pages/basics-page/basics-page.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
 
+interface DataDatePipe {
+  label?: string;
+  format?: string;
+  timezone?: string;
+  locale?: string;
+}
+
 @Component({
   selector: 'app-basics-page',
   templateUrl: './basics-page.component.html',
@@ -12,10 +19,24 @@ export class BasicsPageComponent {
 
   public customDate: Date = new Date();
 
-  data = [
-    {
-      code: 'fecha',
-      render: 'sad'
-    }
-  ]
+  data: DataDatePipe[] = [
+    { label: '', format: '', timezone: '', locale: '' },
+    { label: '', format: 'short', timezone: '', locale: '' },
+    { label: '', format: 'long', timezone: '', locale: '' },
+    { label: '', format: 'MMMM', timezone: '', locale: '' },
+    { label: '', format: 'MMMM dd, yyyy', timezone: '', locale: '' },
+    { label: '', format: 'long', timezone: 'GMT-6', locale: '' },
+    { label: '', format: 'long', timezone: 'GMT-4', locale: '' },
+    { label: '', format: 'long', timezone: '', locale: 'es-HN' },
+    { label: '', format: 'long', timezone: '', locale: 'en-US' },
+    { label: '', format: 'long', timezone: '', locale: 'fr-CA' },
+    { label: 'Hora de Madrid', format: 'hh.mm', timezone: 'GMT+1', locale: '' },
+    { label: 'Hora de Canarias', format: 'hh.mm', timezone: 'GMT+0', locale: '' }
+  ];
+  
+
+  getStringDatePipe({format, timezone, locale}: DataDatePipe): string {
+    if (locale && !timezone) timezone = " ";
+    return ` fecha | date ${format ? `:'${format}'` : ''}${timezone ? ` :'${timezone.trim()}'` : ''}${locale ? ` :'${locale}'` : ''}`;
+  }
 }
