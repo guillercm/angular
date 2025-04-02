89fa5a6ae28b219f1f3ab9cf9c43a1ae90245dca
guille3222 <guillermo.ruiz.cortina.martinez@gmail.com>
2025-04-02 23:14:48 +0200

    .

--- angular/angular/src/app/app.routes.server.ts
+++ angular/angular/src/app/app.routes.server.ts
@@ -2,9 +2,12 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'take-your-foundations-to-the-next-level/pokemon-ssr/pokemons/:id',
+    renderMode: RenderMode.Client,
+  },
+  {
     path: 'angular-from-zero-to-expert/countries/by/:id',
     renderMode: RenderMode.Client,
-
   },
   {
     path: 'angular-from-zero-to-expert/tesloshop/admin/products/:id',
