a67192b7ea9655196beba82b43f8a8085cd3cc17
guillermo-atos <guillermo.ruiz-cortina@atos.net>
2025-04-03 10:29:03 +0200

    .

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,21 +1,21 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
-
+// ServerRoutePrerenderWithParams
 export const serverRoutes: ServerRoute[] = [
   {
     path: 'take-your-foundations-to-the-next-level/pokemon-ssr/pokemons/:id',
-    renderMode: RenderMode.Client,
+    renderMode: RenderMode.Server,
   },
   {
     path: 'angular-from-zero-to-expert/countries/by/:id',
-    renderMode: RenderMode.Client,
+    renderMode: RenderMode.Server,
   },
   {
     path: 'angular-from-zero-to-expert/tesloshop/admin/products/:id',
-    renderMode: RenderMode.Client,
+    renderMode: RenderMode.Server,
   },
   {
     path: 'angular-from-zero-to-expert/tesloshop/product/:idSlug',
-    renderMode: RenderMode.Client,
+    renderMode: RenderMode.Server,
   },
   {
     path: '**',
