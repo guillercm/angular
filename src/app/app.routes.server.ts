2009239303408804344f995c99b4f18d37adb1ca
guillermo-atos <guillermo.ruiz-cortina@atos.net>
2025-04-03 16:54:30 +0200

    .

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -4,26 +4,26 @@ import { PokemonsService } from '@features/take-your-foundations-to-the-next-lev
 
 // ServerRoutePrerenderWithParams
 export const serverRoutes: (ServerRoute | ServerRoutePrerenderWithParams)[] = [
-  // {
-  //   path: 'take-your-foundations-to-the-next-level/pokemon-ssr/pokemons/:id',
-  //   renderMode: RenderMode.Prerender,
-  //   async getPrerenderParams() {
-  //     const pokemonsService = inject(PokemonsService);
-  //     return [...pokemonsService.listIds]; // Generates paths like: [{ id: '1' }, { id: '2' }, { id: '3' }]
-  //   }
-  // },
-  // {
-  //   path: 'angular-from-zero-to-expert/countries/by/:id',
-  //   renderMode: RenderMode.Server,
-  // },
-  // {
-  //   path: 'angular-from-zero-to-expert/tesloshop/admin/products/:id',
-  //   renderMode: RenderMode.Server,
-  // },
-  // {
-  //   path: 'angular-from-zero-to-expert/tesloshop/product/:idSlug',
-  //   renderMode: RenderMode.Server,
-  // },
+  {
+    path: 'take-your-foundations-to-the-next-level/pokemon-ssr/pokemons/:id',
+    renderMode: RenderMode.Prerender,
+    async getPrerenderParams() {
+      const pokemonsService = inject(PokemonsService);
+      return [...pokemonsService.listIds]; // Generates paths like: [{ id: '1' }, { id: '2' }, { id: '3' }]
+    }
+  },
+  {
+    path: 'angular-from-zero-to-expert/countries/by/:id',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'angular-from-zero-to-expert/tesloshop/admin/products/:id',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'angular-from-zero-to-expert/tesloshop/product/:idSlug',
+    renderMode: RenderMode.Server,
+  },
   {
     path: '**',
     renderMode: RenderMode.Prerender
