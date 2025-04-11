4d52f11ab826129e29a1d50a4e76c48430a380de
guille3222 <guillermo.ruiz.cortina.martinez@gmail.com>
2025-04-11 19:21:44 +0200

    .

--- src/environments/environments.prod.ts
+++ src/environments/environments.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   configPath: "static/config.json",
   docsPath: "static/docs/{filename}.md",
-  pdfPath: "/static/pdf/{filename}.pdf",
+  pdfPath: "static/pdf/{filename}.pdf",
   timeoutSeconds: 3,
   playwrightTestsPath: "tests/test-playwright-config.json"
 };
